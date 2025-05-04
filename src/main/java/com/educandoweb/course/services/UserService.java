3b4cab5f3e063cd510b42964ee252f5283c35ab9
Vinicius Damasceno <vinicius.poletti@ucsal.edu.br>
2025-05-04 05:43:08 -0300

    feat: adicionar método de atualização de usuário com PUT no UserResource

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -14,21 +14,34 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
-	public User findById (Long id) {
+
+	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
-	public User insert (User obj) {
+
+	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
-	public void delet (Long id) {
+
+	public void delet(Long id) {
 		repository.deleteById(id);
 	}
- }
+
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+
+	}
+}
