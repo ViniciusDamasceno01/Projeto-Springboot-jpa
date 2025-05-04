99d18346dbe60f011d8f118921131ee886ed00f3
Vinicius Damasceno <vinicius.poletti@ucsal.edu.br>
2025-05-04 00:07:53 -0300

    Implementação da entidade OrderItem com associação muitos-para-muitos contendo atributos extras

--- src/main/java/com/educandoweb/course/entites/OrderItem.java
+++ src/main/java/com/educandoweb/course/entites/OrderItem.java
@@ -4,20 +4,19 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import com.educandoweb.course.entites.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-
-
 
 @Entity
 @Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-    @EmbeddedId
-	private OrderItemPK id;
+	@EmbeddedId
+	private OrderItemPK id = new OrderItemPK();
 
 	private Integer quantity;
 	private Double price;
@@ -33,6 +32,7 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
