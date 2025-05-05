a837daa1f7cb8b3d0e797286861a3c8a1aa69b40
Vinicius Damasceno <vinicius.poletti@ucsal.edu.br>
2025-05-04 21:48:03 -0300

    feat: adicionar tratamento de exceção para operação DELETE

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -48,7 +48,7 @@ public class UserResource {
 	
 	@DeleteMapping (value = "/{id}")
 	public ResponseEntity<Void> delete (@PathVariable Long id) {
-		service.delet(id);
+		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 	
